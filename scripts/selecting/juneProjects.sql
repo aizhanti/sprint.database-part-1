9380e62e83f754f5be8b31dfa9f26a4c43038ad5
cwcraw <cwcraw@gmail.com>
2020-04-15 22:42:15 +0900

    Almost. . .

--- scripts/selecting/juneProjects.sql
+++ scripts/selecting/juneProjects.sql
@@ -1,1 +1,11 @@
 -- Your SQL here
+SELECT
+   DISTINCT ON (projects.name) projects.name
+FROM
+  projects INNER JOIN students_to_projects as sp 
+   ON projects.id = sp.project_id
+  INNER JOIN stt  
+   ON stt.id = sp.student_id
+  INNER JOIN checkins ON stt.id = checkins.student_id
+WHERE 
+     EXTRACT (Month FROM checkins.checked_in_at ) = 6 AND EXTRACT (Year FROM checkins.checked_in_at ) = 2016;
--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,12 @@
 -- Your SQL here
+
+SELECT
+   stt.first_name,
+   stt.last_name
+FROM stt
+   INNER JOIN students_to_projects as sp 
+   ON stt.id = sp.student_id
+  INNER JOIN projects as p 
+   ON p.id = sp.project_id
+WHERE 
+     p.id = '5';
